935e4ff4895d77be8d64e0f357084707319e9e7c
Adam Moss <adamrmoss@gmail.com>
2015-05-13 16:10:44 -0400

    WIP

--- Fuzziverse.Core/Brain.cs
+++ Fuzziverse.Core/Brain.cs
@@ -8,5 +8,16 @@ namespace Fuzziverse.Core
 {
   public class Brain
   {
+    public long Id { get; set; }
+    public long IntensityNeuronId { get; set; }
+    public long AggressivenessNeuronId { get; set; }
+    public long MoveEastNeuronId { get; set; }
+    public long MoveWestNeuronId { get; set; }
+    public long MoveNorthNeuronId { get; set; }
+    public long MoveSouthNeuronId { get; set; }
+    public long Variable1NeuronId { get; set; }
+    public long Variable2NeuronId { get; set; }
+    public long Variable3NeuronId { get; set; }
+    public long Variable4NeuronId { get; set; }
   }
 }
---
+++ Fuzziverse.Core/Neuron.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fuzziverse.Core
+{
+  public class Neuron
+  {
+    public long Id { get; set; }
+    public long BrainId { get; set; }
+  }
+}
