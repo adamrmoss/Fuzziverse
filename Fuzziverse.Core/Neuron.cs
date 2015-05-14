976bb6db834ceeb26acbd8b36c5070ce8df5740a
Adam Moss <adamrmoss@gmail.com>
2015-05-14 00:22:27 -0400

    First hack at data model

--- Fuzziverse.Core/Neuron.cs
+++ Fuzziverse.Core/Neuron.cs
@@ -10,5 +10,21 @@ namespace Fuzziverse.Core
   {
     public long Id { get; set; }
     public long BrainId { get; set; }
+
+    public decimal HealthFactor { get; set; }
+    public decimal DamageFactor { get; set; }
+
+    public decimal IntensityFactor { get; set; }
+    public decimal AggressivenessFactor { get; set; }
+
+    public decimal MoveEastFactor { get; set; }
+    public decimal MoveWestFactor { get; set; }
+    public decimal MoveNorthFactor { get; set; }
+    public decimal MoveSouthFactor { get; set; }
+
+    public decimal Variable1Factor { get; set; }
+    public decimal Variable2Factor { get; set; }
+    public decimal Variable3Factor { get; set; }
+    public decimal Variable4Factor { get; set; }
   }
 }
---
+++ Fuzziverse.Data/Neuron.sql
@@ -0,0 +1,17 @@
+﻿CREATE TABLE dbo.Neuron
+(
+	Id BIGINT NOT NULL PRIMARY KEY IDENTITY, 
+    BrainId BIGINT NOT NULL, 
+    HealthFactor DECIMAL(2, 2) NOT NULL,
+    DamageFactor DECIMAL(2, 2) NOT NULL,
+    IntensityFactor DECIMAL(2, 2) NOT NULL,
+    AggressivenessFactor DECIMAL(2, 2) NOT NULL,
+    MoveEastFactor DECIMAL(2, 2) NOT NULL,
+    MoveWestFactor DECIMAL(2, 2) NOT NULL,
+    MoveNorthFactor DECIMAL(2, 2) NOT NULL,
+    MoveSouthFactor DECIMAL(2, 2) NOT NULL,
+    Variable1Factor DECIMAL(2, 2) NOT NULL,
+    Variable2Factor DECIMAL(2, 2) NOT NULL,
+    Variable3Factor DECIMAL(2, 2) NOT NULL,
+    Variable4Factor DECIMAL(2, 2) NOT NULL,
+)
