18ca74b20f7c1ff44e45667d7e876dab69123034
Adam Moss <adamrmoss@gmail.com>
2015-05-15 18:00:50 -0400

    Uniqueness index for ExperimentTick

--- Fuzziverse.Data/ExperimentTick.sql
+++ Fuzziverse.Data/ExperimentTick.sql
@@ -2,4 +2,9 @@
 	Id BIGINT NOT NULL PRIMARY KEY IDENTITY,
 	ExperimentId BIGINT NOT NULL,
   SimulationTime DATETIME NOT NULL,
+
+  CONSTRAINT UIX_ExperimentTick UNIQUE NONCLUSTERED (
+	  ExperimentId ASC,
+    SimulationTime ASC
+  )
 )
