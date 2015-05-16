5cc62ea7331d8ef0cafcb16c34145e5d71fbb9da
Adam Moss <adamrmoss@gmail.com>
2015-05-15 21:54:57 -0400

    AlienTime

---
+++ Fuzziverse.Core/AlienTime/AlienDateTime.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace Fuzziverse.Core.AlienTime
+{
+  public struct AlienDateTime
+  {
+    public const int TurnsPerPhase = 64;
+    public const int PhasesPerDay = 8;
+
+    public uint Turn { get; set; }
+    public uint Phase { get; set; }
+    public uint Day { get; set; }
+
+    public uint TotalTurns
+    {
+      get { return this.Turn + TurnsPerPhase * (this.Phase + PhasesPerDay * this.Day); }
+    }
+
+    public static AlienDateTime FromTurns(uint totalTurns)
+    {
+      var turn = totalTurns % TurnsPerPhase;
+      var totalPhases = totalTurns / TurnsPerPhase;
+      var phase = totalPhases % PhasesPerDay;
+      var day = totalPhases / PhasesPerDay;
+
+      return new AlienDateTime {
+        Turn = turn,
+        Phase = phase,
+        Day = day,
+      };
+    }
+
+    public static AlienDateTime operator +(AlienTimeSpan alienTimeSpan, AlienDateTime alienDateTime)
+    {
+      return alienDateTime + alienTimeSpan;
+    }
+
+    public static AlienDateTime operator +(AlienDateTime alienDateTime, AlienTimeSpan alienTimeSpan)
+    {
+      var totalTurns = (uint) (alienDateTime.TotalTurns + alienTimeSpan.Turns);
+      return FromTurns(totalTurns);
+    }
+
+    public static AlienDateTime operator -(AlienDateTime alienDateTime, AlienTimeSpan alienTimeSpan)
+    {
+      var totalTurns = (uint) (alienDateTime.TotalTurns - alienTimeSpan.Turns);
+      return FromTurns(totalTurns);
+    }
+
+    public static AlienTimeSpan operator -(AlienDateTime alienDateTime1, AlienDateTime alienDateTime2)
+    {
+      return new AlienTimeSpan {Turns = (int) alienDateTime1.TotalTurns - (int) alienDateTime2.TotalTurns};
+    }
+  }
+
+  public struct AlienTimeSpan
+  {
+    public int Turns { get; set; }
+
+    public static AlienTimeSpan operator +(AlienTimeSpan alienTimeSpan1, AlienTimeSpan alienTimeSpan2)
+    {
+      return new AlienTimeSpan {Turns = alienTimeSpan1.Turns + alienTimeSpan2.Turns};
+    }
+
+    public static AlienTimeSpan operator -(AlienTimeSpan alienTimeSpan1, AlienTimeSpan alienTimeSpan2)
+    {
+      return new AlienTimeSpan {Turns = alienTimeSpan1.Turns - alienTimeSpan2.Turns};
+    }
+  }
+}
---
+++ Fuzziverse.Tests/AlienTime/WhenICompareAlienTime.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Fuzziverse.Core.AlienTime;
+using NUnit.Framework;
+
+namespace Fuzziverse.Tests.AlienTime
+{
+  [TestFixture]
+  public class WhenICompareAlienTime : AssertionHelper
+  {
+    [Test]
+    public void WhenThereIsAPositiveDifference()
+    {
+      var alienTime1 = new AlienDateTime {
+        Day = 0,
+        Phase = 1,
+        Turn = 39,
+      };
+
+      var alienTime2 = new AlienDateTime {
+        Day = 0,
+        Phase = 0,
+        Turn = 5,
+      };
+
+      var alienTimeSpan = alienTime1 - alienTime2;
+      Expect(alienTimeSpan.Turns, this.EqualTo(98));
+    }
+
+    [Test]
+    public void WhenThereIsANegativeDifference()
+    {
+      var alienTime1 = new AlienDateTime {
+        Day = 0,
+        Phase = 1,
+        Turn = 39,
+      };
+
+      var alienTime2 = new AlienDateTime {
+        Day = 1,
+        Phase = 0,
+        Turn = 5,
+      };
+
+      var alienTimeSpan = alienTime1 - alienTime2;
+      Expect(alienTimeSpan.Turns, this.EqualTo(-414));
+    }
+  }
+}
